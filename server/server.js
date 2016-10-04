abf6f8fca3ba697aac70d315fcdaf6f584290757
William Kwan <william@voidupdate.com>
2016-10-04 09:14:25 -0400

    static

--- server/server.js
+++ server/server.js
@@ -6,10 +6,10 @@ var bodyParser = require('body-parser');
 app.use(cors());
 app.use(bodyParser());
 
-var port = Number(process.env.PORT || 5000);
+var port = 5000;
 
 
-
+app.use(express.static('app/public'));
 // var mongoose = require('mongoose');
 // mongoose.connect('mongodb://localhost/jetbrains');
 
@@ -24,14 +24,14 @@ var port = Number(process.env.PORT || 5000);
 //     }
 // });
 
-app.get('/', function (req, res) {
-    console.log("hohohoh");
-    // Product.find(function (err, products) {
-    //     res.send(products);
-    // });
-    // res.send('hello world');
-    res.sendfile('/app/public/index.html');
-});
+// app.get('/', function (req, res) {
+//     console.log("hohohoh");
+//     // Product.find(function (err, products) {
+//     //     res.send(products);
+//     // });
+//     // res.send('hello world');
+//     res.sendfile('/app/public/index.html');
+// });
 
 app.post('/add', function (req, res) {
     // var name = req.body.name;
